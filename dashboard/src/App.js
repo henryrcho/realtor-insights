3d4feab0f18f473bbb7e17f81e0d611d8767e980
Eric Boszin <boszineric@gmail.com>
2020-02-17 12:44:25 -0500

    added basic page routing. next to populate about, tos, and legal

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/home';
+import About from './components/about';
+import TOS from './components/tos';
+import Legal from './components/legal'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-brand">
+              {/* <img src="../../public/logo192.png" width="30" height="30" class="d-inline-block align-top" alt="" /> */}
+              Realtor Insights
+            </li>
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/tos'} className="nav-link">Terms of Service</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+            <li><Link to={'/legal'} className="nav-link">Legal</Link></li>
+          </ul>
+          </nav>
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/tos' component={TOS} />
+              <Route path='/about' component={About} />
+              <Route path='/legal' component={Legal} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
